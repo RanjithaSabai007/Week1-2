c3c0bf23f7a7e1da17a55ee8d74f1a4fa1362ba9
RanjithaSabai007 <ranjithasabai007@gmail.com>
2026-03-23 11:08:44 +0530

    Added p8

--- week1-2/src/p2.java
+++ week1-2/src/p2.java
@@ -1,22 +1,20 @@
-// Problem 7
-import java.util.*;
-class p7 {
-    class T{HashMap<Character,T> c=new HashMap<>(); boolean e;}
-    T root=new T();
-    HashMap<String,Integer> f=new HashMap<>();
+// Problem 8
+class p8 {
+    String[] t; int n;
+    p8(int n){this.n=n;t=new String[n];}
+    int h(String s){return Math.abs(s.hashCode())%n;}
 
-    void insert(String s){
-        f.put(s,f.getOrDefault(s,0)+1);
-        T t=root;
-        for(char ch:s.toCharArray())
-            t=t.c.computeIfAbsent(ch,k->new T());
-        t.e=true;
+    int park(String p){
+        int i=h(p);
+        while(t[i]!=null) i=(i+1)%n;
+        t[i]=p; return i;
     }
 
-    List<String> search(String p){
-        List<String> r=new ArrayList<>();
-        for(String k:f.keySet()) if(k.startsWith(p)) r.add(k);
-        r.sort((a,b)->f.get(b)-f.get(a));
-        return r.subList(0,Math.min(10,r.size()));
+    void exit(String p){
+        int i=h(p);
+        while(t[i]!=null){
+            if(t[i].equals(p)){t[i]=null;return;}
+            i=(i+1)%n;
+        }
     }
 }
