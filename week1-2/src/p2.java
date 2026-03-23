866470990285c386234a32e076d48a8ab4743f2a
RanjithaSabai007 <ranjithasabai007@gmail.com>
2026-03-23 11:08:23 +0530

    Added p7

--- week1-2/src/p2.java
+++ week1-2/src/p2.java
@@ -1,18 +1,22 @@
-// Problem 6
+// Problem 7
 import java.util.*;
-class p6 {
-    class B{int t; long l; B(int m){t=m;l=System.currentTimeMillis();}}
-    HashMap<String,B> map=new HashMap<>();
-    int max=1000,rate=1000;
+class p7 {
+    class T{HashMap<Character,T> c=new HashMap<>(); boolean e;}
+    T root=new T();
+    HashMap<String,Integer> f=new HashMap<>();
 
-    synchronized boolean allow(String id){
-        map.putIfAbsent(id,new B(max));
-        B b=map.get(id);
-        long now=System.currentTimeMillis();
-        int refill=(int)((now-b.l)/3600000.0*rate);
-        b.t=Math.min(max,b.t+refill);
-        b.l=now;
-        if(b.t>0){b.t--;return true;}
-        return false;
+    void insert(String s){
+        f.put(s,f.getOrDefault(s,0)+1);
+        T t=root;
+        for(char ch:s.toCharArray())
+            t=t.c.computeIfAbsent(ch,k->new T());
+        t.e=true;
+    }
+
+    List<String> search(String p){
+        List<String> r=new ArrayList<>();
+        for(String k:f.keySet()) if(k.startsWith(p)) r.add(k);
+        r.sort((a,b)->f.get(b)-f.get(a));
+        return r.subList(0,Math.min(10,r.size()));
     }
 }
